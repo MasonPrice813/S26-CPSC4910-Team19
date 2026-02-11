fc843f555d0b854fc65b55ece4047402d6eb278e
cboz14 <chasebosworth14@gmail.com>
2026-02-11 10:25:38 -0500

    Tweaked SQL script to only store necessary user data

--- user_sql_script.sql
+++ user_sql_script.sql
@@ -1,71 +1,31 @@
--- Table creation of general user information, based on current applicaton information
+USE Team19_DB;
+
 CREATE TABLE users (
-	user_id INT PRIMARY KEY AUTO_INCREMENT,
-    role ENUM('Driver', 'Sponsor', 'Admin') NOT NULL,
-    firstName VARCHAR(50) NOT NULL,
-    lastName VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL,
-    phoneNumber VARCHAR(20),
-    SSN VARCHAR(11),
-    age INT,
-    dateOfBirth DATE,
-    drivingRecord TEXT,
-    criminalHistory TEXT,
-    DLN VARCHAR(50),
-    expirationDate DATE,
-    timeCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  user_id INT PRIMARY KEY AUTO_INCREMENT,
+  role ENUM('Driver', 'Sponsor', 'Admin') NOT NULL,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  email VARCHAR(100) NOT NULL UNIQUE,
+  password VARCHAR(100) NOT NULL,
+  phone_number VARCHAR(20),
+  sponsor VARCHAR(200),
+  time_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Procedure to add a Driver, Sponsor, or Admin information into the table 
 DELIMITER $$
 
 CREATE PROCEDURE add_user (
-    IN p_role ENUM('Driver', 'Sponsor', 'Administrator'),
-    IN p_firstName VARCHAR(50),
-    IN p_lastName VARCHAR(50),
-    IN p_email VARCHAR(100),
-    IN p_password VARCHAR(100),
-    IN p_phoneNumber VARCHAR(20),
-    IN p_SSN VARCHAR(11),
-    IN p_age INT,
-    IN p_dateOfBirth DATE,
-    IN p_drivingRecord TEXT,
-    IN p_criminalHistory TEXT,
-    IN p_DLN VARCHAR(50),
-    IN p_expirationDate DATE
+  IN p_role ENUM('Driver', 'Sponsor', 'Admin'),
+  IN p_first_name VARCHAR(50),
+  IN p_last_name VARCHAR(50),
+  IN p_email VARCHAR(100),
+  IN p_password VARCHAR(100),
+  IN p_phone_number VARCHAR(20),
+  IN p_sponsor VARCHAR(200)
 )
 BEGIN
-    INSERT INTO users (
-        role,
-        firstName,
-        lastName,
-        email,
-        password,
-        phoneNumber,
-        SSN,
-        age,
-        dateOfBirth,
-        drivingRecord,
-        criminalHistory,
-        DLN,
-        expirationDate
-    )
-    VALUES (
-        p_role,
-        p_firstName,
-        p_lastName,
-        p_email,
-        p_password,
-        p_phoneNumber,
-        p_SSN,
-        p_age,
-        p_dateOfBirth,
-        p_drivingRecord,
-        p_criminalHistory,
-        p_DLN,
-        p_expirationDate
-    );
+  INSERT INTO users (role, first_name, last_name, email, password, phone_number, sponsor)
+  VALUES (p_role, p_first_name, p_last_name, p_email, p_password, p_phone_number, p_sponsor);
 END$$
 
 DELIMITER ;
